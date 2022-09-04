5b7f2fef1dbcaa7fc444012f4d9b2fb43daf40cb
monikamaric98 <monika.maric@fpmoz.sum.ba>
2022-09-04 20:19:00 +0200

    zavrsavanje

--- routes/web.php
+++ routes/web.php
@@ -36,21 +36,26 @@ Route::get('/users/delete/{id}', [UserController::class, 'deleteuser'])->name('u
 Route::get('/user{user}edit', [UserController::class, 'editrole'])->name('user.editrole');
 Route::put('/user/update/{user}', [UserController::class, 'changerole']);
 
-Route::get('/saloni', [SalonController::class, 'index']);
-Route::post('/addsalon', [SalonController::class, 'add'])->name('salons.add');
-Route::get('/saloni{salon}', [SalonController::class, 'show']);
-Route::get('/salon{salon}edit', [SalonController::class, 'edit'])->name('salons.edit');
-Route::put('/saloni/update/{salon}', [SalonController::class, 'update']);
-Route::get('/saloni/delete/{id}', [SalonController::class, 'delete'])->name('salons.delete');
+Route::middleware(['auth'])->group(function () {
+    Route::get('/saloni', [SalonController::class, 'index']);
 
-Route::get('/termini', [TerminController::class, 'index']);
-Route::get('/mojitermini', [TerminController::class, 'mojitermini']);
-Route::post('/addtermin', [TerminController::class, 'create'])->name('termins.add');
-Route::get('/termindelete{termin}', [TerminController::class, 'termindelete']);
-Route::get('/termin/delete/{id}', [TerminController::class, 'destroy'])->name('termin.delete');
-Route::get('/taketermin{termin}', [TerminController::class, 'taketermin']);
-Route::put('/termins/isAvailable/{id}', [TerminController::class, 'changeIsAvailable']);
-Route::get('/otkazitermin{termin}', [TerminController::class, 'otkazitermin']);
-Route::get('/termins/otkazi/{id}', [TerminController::class, 'changeAvailability'])->name('termins.change');
-Route::get('/search/', [TerminController::class, 'search'])->name('search');
+    Route::post('/addsalon', [SalonController::class, 'add'])->name('salons.add');
+    Route::get('/saloni{salon}', [SalonController::class, 'show']);
+    Route::get('/salon{salon}edit', [SalonController::class, 'edit'])->name('salons.edit');
+    Route::put('/saloni/update/{salon}', [SalonController::class, 'update']);
+    Route::get('/saloni/delete/{id}', [SalonController::class, 'delete'])->name('salons.delete');
+    Route::get('/termini', [TerminController::class, 'index']);
+    Route::get('/mojitermini', [TerminController::class, 'mojitermini']);
+    Route::post('/addtermin', [TerminController::class, 'create'])->name('termins.add');
+    Route::get('/termindelete{termin}', [TerminController::class, 'termindelete']);
+    Route::get('/termin/delete/{id}', [TerminController::class, 'destroy'])->name('termin.delete');
+    Route::get('/taketermin{termin}', [TerminController::class, 'taketermin']);
+    Route::put('/termins/isAvailable/{id}', [TerminController::class, 'changeIsAvailable']);
+    Route::get('/otkazitermin{termin}', [TerminController::class, 'otkazitermin']);
+    Route::get('/termins/otkazi/{id}', [TerminController::class, 'changeAvailability'])->name('termins.change');
+    Route::get('/search/', [TerminController::class, 'search'])->name('search');
+});
+
+
+
 
